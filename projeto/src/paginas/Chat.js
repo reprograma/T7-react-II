b69844b14595b71bbf6909f30b6307e5bdcf755b
Luciana <lprimante@gmail.com>
2019-06-19 16:58:45 -0300

    Projeto com chat e comentarios

--- projeto/src/paginas/Chat.js
+++ projeto/src/paginas/Chat.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Comentario from '../componentes/comentario/Comentario';
+import { getComentarios } from '../service/base'
+import './chat.css'
 
-export default function Chat(props) {
-    return(
-        <p>Você está no Chat!</p> 
-    )
+export default class Chat extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            comentarios: new Array(),
+        }
+    }
+
+    componentDidMount() {
+        getComentarios()
+        .then(response => {
+            console.log(response)
+            this.setState({
+                comentarios: response.data
+            })
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+
+    render(){
+        const { comentarios } = this.state
+        return(
+            <div className='chat'>
+                {
+                    comentarios.length > 0 ?
+                        comentarios.map((item, index) => {
+                            return <Comentario comentario={item} key={index+'comentario'}/>
+                        })
+                        :
+                        <span>Carregando comentários :D</span>
+                }
+            </div>
+        )
+    }
 }
